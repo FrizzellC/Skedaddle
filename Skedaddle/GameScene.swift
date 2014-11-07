ee7005d886ad3e9e29859aae94afe0a62d8c2c85
Chantel <chantelfrizzell@hotmail.com>
2014-11-06 18:30:24 -0800

    First commit

--- Skedaddle/GameScene.swift
+++ Skedaddle/GameScene.swift
@@ -8,38 +8,60 @@
 
 import SpriteKit
 
+        var ship = SKSpriteNode()
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!";
+//        myLabel.fontSize = 65;
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+//        
+//        self.addChild(myLabel)
+//    
+//    }
+//    
+//    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+//        /* Called when a touch begins */
+//    }
+//    
+//   
+//    override func update(currentTime: CFTimeInterval) {
+//        /* Called before each frame is rendered */
+//    }
+
+
+//Phil's ship
+
+var shipTexture1 = SKTexture(imageNamed: "ship01")
+shipTexture1.filteringMode = SKTextureFilteringMode.Nearest
+var shipTexture2 = SKTexture(imageNamed: "ship02")
+shipTexture1.filteringMode = SKTextureFilteringMode.Nearest
+var shipTexture3 = SKTexture(imageNamed: "ship03")
+shipTexture1.filteringMode = SKTextureFilteringMode.Nearest
+var shipTexture4 = SKTexture(imageNamed: "ship04")
+
+/* animate the ship*/
+var animation = SKAction.animateWithTextures([shipTexture1, shipTexture2, shipTexture3, shipTexture4], timePerFrame: 0.2)
+var engineGo = SKAction.repeatActionForever(animation)
+
+/* set the ship's position relative to the frame: */
+ship = SKSpriteNode(texture: shipTexture1)
+ship.setScale(0.5)
+ship.position = CGPoint(x: self.frame.size.width * 0.35, y: self.frame.size.height * 0.6)
+ship.runAction(engineGo)
+
+/* add ship's physics */
+ship.physicsBody = SKPhysicsBody(circleOfRadius: ship.size.height / 2.0)
+ship.physicsBody?.dynamic = true
+ship.physicsBody?.allowsRotation = false
+
+
+
+/* add bird to the scene: */
+self.addChild(ship)
+
+}
 }
